c4895edc6d6fa241101f0f4f499982477fd83d2c
rgprince <rgprince@users.noreply.github.com>
2025-11-26 17:11:46 +0530

    Fix: Add back withContext import

--- app/src/main/kotlin/com/rgprince/nayanai/model/ModelConverter.kt
+++ app/src/main/kotlin/com/rgprince/nayanai/model/ModelConverter.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 import java.io.File
 
 sealed class ConversionState {
